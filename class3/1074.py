3e0f60a6a6b64fcc6d09121128533835bd5808dc
DohyunJegal <80775466+DohyunJegal@users.noreply.github.com>
2023-04-17 15:37:22 +0900

    20230417

---
+++ class3/1074.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+res = 0
+
+while n != 0:
+    n -= 1
+
+    # 사분면으로 나눠서 계산
+    if r < 2**n and c < 2**n:
+        # 사분면에 해당되는 칸 수를 결과값에 더해줌
+        res += (2**n) * (2**n) * 0
+    elif r < 2**n and c >= 2**n:
+        res += (2**n) * (2**n) * 1
+        # 줄어든 영역에 맞춰 좌표 보정
+        c -= 2**n
+    elif r >= 2 ** n and c < 2 ** n:
+        res += (2**n) * (2**n) * 2
+        r -= 2**n
+    else:
+        res += (2**n) * (2**n) * 3
+        c -= 2**n
+        r -= 2**n
+
+print(res)
